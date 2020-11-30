5287e3f22f0f20c0589e85c4058ce7c0a050d8b2
Othmane Alaoui M'Daghri <othmane.alaoui-mdaghri@azeo.com>
2020-11-30 16:26:35 +0100

    delete sudo

--- installation.sh
+++ installation.sh
@@ -4,21 +4,21 @@ then
     echo Docker already installed
 else
     echo Installing docker
-    sudo apt-get remove docker docker-engine docker.io containerd runc
-    sudo apt-get update
-    sudo apt-get install \
+    apt-get remove docker docker-engine docker.io containerd runc
+    apt-get update
+    apt-get install \
         apt-transport-https \   
         ca-certificates \
         curl \
         gnupg-agent \
         software-properties-common
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-    sudo add-apt-repository \
+    add-apt-repository \
         "deb [arch=amd64] https://download.docker.com/linux/debian \
         $(lsb_release -cs) \
         stable"
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+    apt-get update
+    apt-get install docker-ce docker-ce-cli containerd.io -y
 fi
 
 name='container_onnx'
